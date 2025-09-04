0dd9b31837e83962bcfa6e8c0c89e949f1b262d4
haiqalzaki <haiqalzaki.hz@gmail.com>
2025-09-04 09:56:52 +0800

    added UI offline resources (charts, icons, fonts) and edited head partials

--- App/Views/Partials/head-main.php
+++ App/Views/Partials/head-main.php
@@ -6,12 +6,12 @@
     <title><?= $data['tajuk'] ?> Page</title>
     <link rel="icon" type="image/x-icon" href="<?= $_ENV['HOME_URL'] ?>/image/matin.ico">
     <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/library/bootstrap/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/css/font.css">
     <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/css/<?= $data['css'] ?>.css">   
+    <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/library/bootstrap-icons/css/bootstrap-icons.css">
     <script src="<?= $_ENV['HOME_URL'] ?>/js/helper/constant.js"></script>
     <script src="<?= $_ENV['HOME_URL'] ?>/js/helper/function.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="<?= $_ENV['HOME_URL'] ?>/library/chartjs/chart.umd.min.js"></script>
 </head>
 <style>
 body, html {
--- App/Views/Partials/head.php
+++ App/Views/Partials/head.php
@@ -6,18 +6,17 @@
     <title><?= $data['tajuk'] ?> Page</title>
     <link rel="icon" type="image/x-icon" href="<?= $_ENV['HOME_URL'] ?>/image/matin.ico">
     <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/library/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"> <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/library/datatables/datatables.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/css/font.css">
     <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/css/navbar.css">
     <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/css/sidebar.css">
     <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/css/<?= $data['css'] ?>.css">     
+    <link rel="stylesheet" href="<?= $_ENV['HOME_URL'] ?>/library/bootstrap-icons/css/bootstrap-icons.css">
     <script src="<?= $_ENV['HOME_URL'] ?>/js/helper/constant.js"></script>
     <script src="<?= $_ENV['HOME_URL'] ?>/js/helper/config.js"></script>
     <script src="<?= $_ENV['HOME_URL'] ?>/js/helper/function.js"></script>
     <script src="<?= $_ENV['HOME_URL'] ?>/library/table.js"></script>
     <script src="<?= $_ENV['HOME_URL'] ?>/library/datatables/datatables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="<?= $_ENV['HOME_URL'] ?>/library/chartjs/chart.umd.min.js"></script>
 </head>
 <body>
