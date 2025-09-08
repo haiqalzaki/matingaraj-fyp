4bb83452e254b7439490ac84d79776f821f32068
haiqalzaki <haiqalzaki.hz@gmail.com>
2025-09-08 23:45:01 +0800

    adjust reset page

--- App/Views/Templates/User/reset.php
+++ App/Views/Templates/User/reset.php
@@ -13,28 +13,32 @@
                             <span class="text-success"><?= $data['userName'] ?></span>
                         </p>
                     </div> 
-                    <div class="d-flex justify-content-center align-items-center">
-                        <form class="form-control w-100 w-md-75 w-lg-50" id="reset-password-form" autocomplete="off">
-                            <div class="mb-2">
-                                <label for="user-password-new" class="form-label">New Password: </label>
-                                <input type="password" class="form-control" name="userNewPassword" id="user-password-new" placeholder="(Min. 10 characters)">
-                            </div>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-md-8 col-lg-6">
+                                <form class="form-control w-100" id="reset-password-form" autocomplete="off">
+                                    <div class="mb-2">
+                                        <label for="user-password-new" class="form-label">New Password: </label>
+                                        <input type="password" class="form-control" name="userNewPassword" id="user-password-new" placeholder="(Min. 10 characters)">
+                                    </div>
 
-                            <div class="mb-2">
-                                <label for="user-password-confirm-new" class="form-label">Confirm New Password: </label>
-                                <input type="password" class="form-control" id="user-password-confirm-new" placeholder="(Min. 10 characters)">
-                            </div>
-                    
-                            <div class="mb-2">
-                                <label for="user-recovery-key" class="form-label">Recovery Key: </label>
-                                <input type="password" class="form-control" name="userKey" id="user-recovery-key" placeholder="Enter secret key here">
-                            </div>
+                                    <div class="mb-2">
+                                        <label for="user-password-confirm-new" class="form-label">Confirm New Password: </label>
+                                        <input type="password" class="form-control" id="user-password-confirm-new" placeholder="(Min. 10 characters)">
+                                    </div>
+                            
+                                    <div class="mb-2">
+                                        <label for="user-recovery-key" class="form-label">Recovery Key: </label>
+                                        <input type="password" class="form-control" name="userKey" id="user-recovery-key" placeholder="Enter secret key here">
+                                    </div>
 
-                            <div class="d-flex justify-content-center mt-3 mb-2">
-                                <input type="hidden" name="userID" id="user-id-reset" value="<?= $data['userId'] ?>">
-                                <button type="button" class="btn btn-success w-md-50 w-lg-25" id="reset-pswd-btn">Submit</button>
+                                    <div class="d-flex justify-content-center mt-3 mb-2">
+                                        <input type="hidden" name="userID" id="user-id-reset" value="<?= $data['userId'] ?>">
+                                        <button type="button" class="btn btn-success w-md-50 w-lg-25" id="reset-pswd-btn">Submit</button>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
