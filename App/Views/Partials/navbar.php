160fd2e6e52f3c7ab0fdf4084266974ca2ed4f60
haiqalzaki <haiqalzaki.hz@gmail.com>
2025-09-13 01:46:10 +0800

    edited navbar elements

--- App/Views/Partials/navbar.php
+++ App/Views/Partials/navbar.php
@@ -1,19 +1,15 @@
 <!-- Navbar Section -->
 <nav class="nav_bar navbar navbar-expand-sm border-bottom shadow sticky-top d-none d-md-block" style="background-color: #ffff">
-  <div class="container-fluid d-flex justify-content-between align-self-center">
-    <div>
-        <h3 class="ms-3">
-            
-        </h3>
+    <div class="container-fluid d-flex justify-content-between align-self-center">
+        <div>
+            <h5 class="ms-3">
+
+            </h5>
+        </div>
+        <div>
+            <h3 class="me-2">
+                <span class="fs-4 fw-light">Hi, <strong><?= $_SESSION['username'] ?></strong> !</span>
+            </h3>
+        </div>
     </div>
-    <div>
-        <h3 class="me-2">
-            <span class="fs-4 fw-light">Hi, <strong><?= $_SESSION['username'] ?></strong> !</span>
-        </h3>
-    </div>
-  </div>
 </nav>
-
-
-
-
