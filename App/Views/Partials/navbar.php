32ceaeeeb24665ae1724b1efc98dc970b3844b50
haiqalzaki <haiqalzaki.hz@gmail.com>
2025-09-13 01:35:02 +0800

    edited navbar to greet user with related session name

--- App/Views/Partials/navbar.php
+++ App/Views/Partials/navbar.php
@@ -1,9 +1,14 @@
 <!-- Navbar Section -->
 <nav class="nav_bar navbar navbar-expand-sm border-bottom shadow sticky-top d-none d-md-block" style="background-color: #ffff">
-  <div class="container-fluid d-flex justify-content-around">
-    <div class="align-self-center">
-        <h3 class="pb-2 pt-3 text-uppercase">
-            <?= $data['tajuk'] ?> VIEW
+  <div class="container-fluid d-flex justify-content-between align-self-center">
+    <div>
+        <h3 class="pb-2 pt-3 ms-3 text-uppercase">
+            <?= $data['tajuk'] ?>
+        </h3>
+    </div>
+    <div>
+        <h3 class="me-2">
+            <span class="fs-4 fw-light">Hi, <strong><?= $_SESSION['username'] ?></strong> !</span>
         </h3>
     </div>
   </div>
